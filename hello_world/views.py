a809773ee5c003cf815494794f6c8369298ae925
Timea Farkas <tmeafarkas@yahoo.com>
2024-07-04 17:47:30 +0000

    first lesson

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    if request.method == "POST":
+        return HttpResponse("POSTed something?")
+    else:
+        return HttpResponse(request.method)
